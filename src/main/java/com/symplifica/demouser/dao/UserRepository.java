4a17c969e84ad008e3de1616e2f61799a07d88b3
Zabdiel Elias <zabdieljr2@gmail.com>
2023-10-09 17:33:53 -0500

    "close #15"

--- src/main/java/com/symplifica/demouser/dao/UserRepository.java
+++ src/main/java/com/symplifica/demouser/dao/UserRepository.java
@@ -2,6 +2,9 @@ package com.symplifica.demouser.dao;
 
 import com.symplifica.demouser.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource (path = "registered_users")
 public interface UserRepository extends JpaRepository<User,Integer> {
 }
